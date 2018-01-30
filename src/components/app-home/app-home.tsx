b3b05efb9bafc0cf17fe8d48c3dda421883d17c2
KARTHIKEYAN <karthikeyanr@365media.in>
2018-01-30 16:24:26 +0530

    This is to show the script tag not cmpiled error during production build

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,12 +1,36 @@
 import { Component } from '@stencil/core';
-
+declare var google: any;
 
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.scss'
 })
 export class AppHome {
+  componentDidLoad() {
+    this.createPieChart('piechart');
+  }
+  createPieChart(element) {
+    google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Events', 'Run an event count'],
+          ['Politics',     11],
+          ['Sports',      2],
+          ['Travels',  2]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById(element));
+
+        chart.draw(data, options);
+      }
+  }
   render() {
     return (
       <div>
@@ -16,7 +40,7 @@ export class AppHome {
           web components using Stencil!
           Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
         </p>
-
+        <div class="chart-wrapper" id="piechart"></div>
         <stencil-route-link url='/profile/stencil'>
           <button>
             Profile page
